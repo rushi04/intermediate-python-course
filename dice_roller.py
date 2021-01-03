c73976c5c9b576585bed11bb8a66e197cb5b83ed
Rushikesh Ghule <rushirocks2136@gmail.com>
2021-01-04 00:27:13 +0530

    conditionals

--- dice_roller.py
+++ dice_roller.py
@@ -4,8 +4,13 @@ def main():
   dice_sum = 0
   for i in range(0,dice_rolls):
     roll = random.randint(1,6)
-    print(f'You rolled a {roll}')
-    dice_sum+=roll
+    #print(f'You rolled a {roll}')
+    if roll==1:
+      print(f'You rolled a {roll}! Critical Fail')
+    elif roll==6:
+      print(f'You rolled a {roll}! Critical Success!')
+    else:
+      dice_sum+=roll
   print(f'Your score is {dice_sum}')
 
 
