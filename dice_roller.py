63576f020058073e130a6f179bc9c081ed82f8f8
Rushikesh Ghule <rushirocks2136@gmail.com>
2021-01-04 00:23:22 +0530

    now rolling random no of dices

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
+import random
 def main():
-  print('You rolled a die')
+  dice_rolls = random.randint(1,100)
+  dice_sum = 0
+  for i in range(0,dice_rolls):
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
+    dice_sum+=roll
+  print(f'Your score is {dice_sum}')
+
 
 if __name__== "__main__":
   main()
